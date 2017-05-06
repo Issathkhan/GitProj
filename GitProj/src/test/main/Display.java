e31cb476dbb2cdaa44643a257f45524b74ae182b
Issath <onissathkhan@gmail.com>
2017-05-07 03:02:12 +0530

    Print class added

---
+++ GitProj/src/test/main/Display.java
@@ -0,0 +1,9 @@
+//$Id$
+package test.main;
+
+public class Display {
+	public void print(String msg) {
+		System.out.println("Data:"+msg);
+
+	}
+}
--- GitProj/src/test/main/TestMain.java
+++ GitProj/src/test/main/TestMain.java
@@ -5,7 +5,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		int arr[] = { 1, 2, 3, 4, 5};
 
-		for(int xy : arr){//test conflict
+		for(int xy : arr){//test conflict files
 			System.out.println("Newest Data:"+xy);
 		}
 		System.out.println("Welcome!");
