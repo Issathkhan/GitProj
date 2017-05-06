a9da3ed86f2e3a2291d19edf99850d94e4135f10
Issath <onissathkhan@gmail.com>
2017-05-07 00:38:05 +0530

    Initial checkin for test

---
+++ GitProj/src/test/main/TestMain.java
@@ -0,0 +1,11 @@
+//$Id$
+package test.main;
+
+public class TestMain {
+	public static void main(String[] args) {
+		int arr[] = {1,2,3};
+		for(int xy : arr){
+			System.out.println("Print Value:"+xy);
+		}
+	}
+}
